fec57e58ca64344905820f64b5fbbfea3f4789d7
AbdulMoiz8994 <63612568+AbdulMoiz8994@users.noreply.github.com>
2020-10-13 15:23:52 +0500

    add these three files chnages color

--- src/Child.js
+++ src/Child.js
@@ -1,15 +1,18 @@
-import React,{useContext} from 'react';
+import React,{useContext,useState} from 'react';
 import CreateCounter from './CreateCounter.js';
 
 const Child = (props) =>{
     let counterValue=useContext(CreateCounter);
+    let [isMorning, isnight]=useState();
 
 
     return(
-<div>
+<div className={` ${isMorning? 'night' : 'day'}`}>
 <h1>hello abdul moiz we are on now {props.name} </h1> 
     <h2>We are doing the counting via ContextAPI: {counterValue[0]}</h2>
     <button onClick={() => counterValue[1](++counterValue[0])}>Incremnt Context</button>
+    <button onClick={() => isnight(!isMorning)}>Chnage day</button>
+
 </div>
     );
 }
--- src/Child2.js
+++ src/Child2.js
@@ -1,13 +1,16 @@
-import React,{useReducer} from 'react';
+import React,{useReducer ,useState} from 'react';
 import CounterReducer from './CounterReducer.js';
-
+import './App.js';
 
 const Child2= () =>{
 let [setValue, incrementValue]=useReducer(CounterReducer, 1);
+let [isMorning, isnight]=useState();
     return(
-<div>
+<div className={`box ${isMorning? 'morning' : ''}`}>
     <h1>Hello moiz we are doing increment every time via reducer: {setValue}</h1>
     <button onClick={() => incrementValue('INCREMENT')}>IncremntReducer</button>
+    <button onClick={() => isnight(!isMorning)}>Chnage day</button>
+    
 </div>
     );
 }
